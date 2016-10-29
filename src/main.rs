d709c04e584a7b261c071730e21a6c7eb16e2019
SLMT <sam123456777@gmail.com>
2016-10-29 15:59:13 +0800

    We can create a window now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow = WindowSettings::new("Hello Meow!!",
+        [640, 480]).exit_on_esc(true).build().unwrap();
+
+    while let Some(event) = window.next() {
+        
+    }
 }
