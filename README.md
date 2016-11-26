d58d8a5358d0b25a58b3c4a4ec20da5aff33f459
SLMT <sam123456777@gmail.com>
2016-11-26 23:38:30 +0800

    Foods are not generated on top of the snake now

--- README.md
+++ README.md
@@ -16,11 +16,11 @@ The executable binary will appear in `target/debug`, called `rust-snake.exe`.
 
 - ~~To randomly generate foods~~
 - ~~To make the snake move itself~~
-- To make the snake die when it hits itself
+- ~~To make the snake die when it hits itself~~
 - ~~To make the snake die when it hits borders~~
-- To make the game restart after the snake dies
+- ~~To make the game restart after the snake dies~~
 - To have a better game-over screen
 
 ## Bugs
 
-- Foods should not be shown up on top of the snake
+- ~~Foods should not be shown up on top of the snake~~
--- src/game.rs
+++ src/game.rs
@@ -127,8 +127,18 @@ impl Game {
 
     fn add_food(&mut self) {
         let mut rng = thread_rng();
-        self.food_x = rng.gen_range(1, self.width - 1);
-        self.food_y = rng.gen_range(1, self.height - 1);
+
+        // Decide the position of the new food
+        let mut new_x = rng.gen_range(1, self.width - 1);
+        let mut new_y = rng.gen_range(1, self.width - 1);
+        while self.snake.is_overlap_except_tail(new_x, new_y) {
+            new_x = rng.gen_range(1, self.width - 1);
+            new_y = rng.gen_range(1, self.width - 1);
+        }
+
+        // Add the new food
+        self.food_x = new_x;
+        self.food_y = new_y;
         self.food_exist = true;
     }
 
